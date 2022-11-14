7396443ec82871414fb3517a80a1c35707362cb9
Manuel Serafin <manuel.serafin@outlook.com>
2022-11-13 23:48:41 -0300

    framework change (bootstrap), font size changed to rem unit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IGitHubResponse, useFetch } from "../components/useFetch";
-import logo from "../assets/image/logo/day/nina_logo_black.webp";
+import logo from "../assets/image/logo/day/nina_logo_black.png";
 import "../styles.css";
 
 export const Home = () => {
@@ -10,38 +10,38 @@ export const Home = () => {
   };
   return (
     <>
-      <main className="hero">
-        <section className="container">
-          <div className="columns is-justify-content-space-between">
-            <div className="column p-0 is-half is-flex is-flex-direction-column is-justify-content-space-between">
+      <main id="mainHome" className="container">
+        <section className="container d-flex flex-column justify-content-between">
+          <div className="row d-flex justify-content-between mainHomeRow1">
+            <div className="col-sm-6">
               <img
                 src={logo}
                 title="Are you seeing the cat?"
-                className="animate__bounceIn"
+                className="img-fluid animate__bounceIn"
                 alt="Nina company logo"
               />
-              <div>
-                <h2 className="small-title-bold">
-                  Find more releases in GitHub
-                </h2>
-                <h2 className="small-subtitle">
-                  Nina 2022 | Francisco Possetto
-                </h2>
-              </div>
             </div>
-            <div className="column p-0 is-one-third is-flex is-flex-direction-column is-justify-content-space-between">
+            <div className="col-sm-4">
               <h1 className="main-description">
                 A collection of tiny tools for Autodesk Revit.
               </h1>
-              <div>
-                <h2 className="small-title">Download for Windows</h2>
-                <button
-                  className="button is-rounded is-black small-title-btn"
-                  onClick={download}
-                >
-                  Latest Release<strong> {release?.name}-beta </strong>
-                </button>
-              </div>
+            </div>
+          </div>
+          {/*  */}
+          <div className="row d-flex justify-content-between align-items-end mainHomeRow2">
+            <div className="col-sm-4">
+              <h2 className="small-title-bold">Find more releases in GitHub</h2>
+              <h2 className="small-subtitle">Nina 2022 | Francisco Possetto</h2>
+            </div>
+            <div className="col-sm-4">
+              <h2 className="small-title">Download for Windows</h2>
+              <button
+                type="button"
+                className="btn btn-dark small-title-btn"
+                onClick={download}
+              >
+                Latest Release<strong> {release?.name}-beta </strong>
+              </button>
             </div>
           </div>
         </section>
