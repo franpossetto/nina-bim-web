14ba69333aecb594991a631cbbf255225f11f7d0
Manuel Serafin <manuel.serafin@outlook.com>
2022-11-17 00:28:51 -0300

    Responsive work starts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,20 +9,20 @@ export const Home = () => {
     window.location.assign(link);
   };
 
-  const screenIsSmall= () => {
-    if (window.innerWidth <= 1536) {
-      return true;
-    } else {
-      return false
-    }
-  }
+  // const screenIsSmall = () => {
+  //   if (window.innerWidth <= 1536) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // };
 
   return (
     <>
       <main id="mainHome" className="container">
-        <section className="container d-flex flex-column justify-content-between">
+        <section className="container d-flex flex-column justify-content-between mainHomeContainer">
           <div className="row d-flex justify-content-between mainHomeRow1">
-            <div className="col-sm-6">
+            <div className="col-sm-6 mainHomeImg">
               <img
                 src={logo}
                 title="Are you seeing the cat?"
@@ -30,19 +30,20 @@ export const Home = () => {
                 alt="Nina company logo"
               />
             </div>
-            <div className="col-sm-4">
-              <h1 className={screenIsSmall() ? "main-description small-text" : "main-description big-text"}>
+            <div className="col-sm-4 mainHomeDescription">
+              {/* <h1 className={screenIsSmall() ? "main-description small-text" : "main-description big-text"}> */}
+              <h1 className="mainDescription">
                 A collection of tiny tools for Autodesk Revit.
               </h1>
             </div>
           </div>
           {/*  */}
           <div className="row d-flex justify-content-between align-items-end mainHomeRow2">
-            <div className="col-sm-4">
+            <div className="col-sm-4 mainHomeTitle">
               <h2 className="small-title-bold">Find more releases in GitHub</h2>
               <h2 className="small-subtitle">Nina 2022 | Francisco Possetto</h2>
             </div>
-            <div className="col-sm-4">
+            <div className="col-sm-4 mainHomeDownload">
               <h2 className="small-title">Download for Windows</h2>
               <button
                 type="button"
